a01a14a70dda7b640b2a3420b7c25c039e9832fa
Ruhumbika <ruhumbikamtumbajohn@gmail.com>
2024-08-15 18:02:03 +0300

    Add files via upload

--- connect.php
+++ connect.php
@@ -1,16 +1,8 @@
 <?php
-// connect.php
 
-$servername = "localhost"; // Replace with your server name
-$username = "root";        // Replace with your database username
-$password = "";            // Replace with your database password
-$dbname = "rjaywebsite"; // Replace with your database name
+$con=new mysqli('localhost', 'root','','crudopperation');
 
-// Create connection
-$conn = mysqli_connect($servername, $username, $password, $dbname);
-
-// Check connection
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
+if(!$con){
+    die(mysqli_error($con));
 }
 ?>
